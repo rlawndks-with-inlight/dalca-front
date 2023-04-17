d51c29a56b6b3310f029ed8528761bd8dded94ee
rlawndks4 <rlawndks0423@naver.com>
2023-04-17 11:23:49 +0900

    asdf

--- src/data/Manager/ManagerContentData.js
+++ src/data/Manager/ManagerContentData.js
@@ -188,7 +188,7 @@ export const objManagerListContent = {
         [
             columnObjFormat('문의자아이디', '', 'text', 'id'),
             columnObjFormat('문의자명', '', 'text', 'name'),
-            columnObjFormat('문의자주민번호', '', 'text', 'id_number'),
+           // columnObjFormat('문의자주민번호', '', 'text', 'id_number'),
             columnObjFormat('제목', '', 'text', 'title'),
             columnObjFormat('확인여부', '', 'request_status', 'request_status'),
             columnObjFormat('문의날짜', '', 'text', 'date'),
