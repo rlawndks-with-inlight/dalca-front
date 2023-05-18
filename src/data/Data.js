6302ad592966caa0451f1192b75f7a9f0e8b4a02
rlawndks4 <rlawndks0423@naver.com>
2023-05-18 20:59:34 +0900

    asfd

--- src/data/Data.js
+++ src/data/Data.js
@@ -27,10 +27,10 @@ import io from 'socket.io-client';
 
 const test = true;
 
-export const backUrl = "http://localhost:8001";
-//export const backUrl = "https://dalcapay.com:8443";
-//export const frontUrl = "https://dalcapay.com";
-export const frontUrl = "http://localhost:2000";
+//export const backUrl = "http://localhost:8001";
+export const backUrl = "https://dalcapay.com:8443";
+export const frontUrl = "https://dalcapay.com";
+//export const frontUrl = "http://localhost:2000";
 //export const backUrl = "https://1st-academy.kr:8443";
 export const SOCKET_URL = `https://dalcapay.com:5000`; // https://dalcapay.com:5000
 export const socket = io(SOCKET_URL);
